df5c5c89f2d22cedc55ae8d50a909c65eff65845
Jan eudald VAQUER <jvaquer@e1r10p23.42.fr>
2020-07-20 18:56:59 +0200

    push

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 echo "Deleting previous Minikube elements."
 minikube delete
 echo "Starting minikube..."
-minikube --vm-driver=docker start
+minikube --vm-driver=docker start --extra-config=apiserver.service-node-port-range=1-35000
 #minikube --vm-driver=virtualbox start --extra-config=apiserver.service-node-port-range=1-35000
 
 #enable addons
@@ -11,32 +11,45 @@ minikube addons enable ingress
 minikube addons enable dashboard
 echo "Addons enabled successfully"
 
+echo "Launching dashboard..."
+minikube dashboard &
+
 #set up docker env
 echo "Setting up env..."
 eval $(minikube docker-env)
 echo "Env set up correctly"
 
 #stockage of minikube ip
-MINIKUBE_IP=`minikube ip`
-echo "minikube ip: $MINIKUBE_IP"
+#MINIKUBE_IP=`minikube ip`
+#echo "minikube ip: $MINIKUBE_IP"
+IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
+printf "Minikube IP: ${IP}"
 
 #config setup
 echo "Setting up config files..."
-cp nginx/index_model.html nginx/index.html
-cp ftps/start_model.sh ftps/start.sh
-cp mysql/wordpress_model.sql mysql/wordpress.sql
-cp telegraf/telegraf_model.conf telegraf/telegraf.conf
+cp src/nginx/index_model.html src/nginx/index.html
+#cp src/ftps/start_model.sh src/ftps/start.sh
+#cp src/mysql/wordpress_model.sql src/mysql/wordpress.sql
+#cp src/telegraf/telegraf_model.conf src/telegraf/telegraf.conf
 
-sed -i 's/MINIKUBE_IP/'"$MINIKUBE_IP"'/g' nginx/index.html
-sed -i 's/MINIKUBE_IP/'"$MINIKUBE_IP"'/g' ftps/start.sh
-sed -i 's/MINIKUBE_IP/'"$MINIKUBE_IP"'/g' mysql/wordpress.sql
-sed -i 's/MINIKUBE_IP/'"$MINIKUBE_IP"'/g' telegraf/telegraf.conf
+sed -i 's/MINIKUBE_IP/'"$MINIKUBE_IP"'/g' src/nginx/index.html
+#sed -i 's/MINIKUBE_IP/'"$MINIKUBE_IP"'/g' src/ftps/start.sh
+#sed -i 's/MINIKUBE_IP/'"$MINIKUBE_IP"'/g' src/mysql/wordpress.sql
+#sed -i 's/MINIKUBE_IP/'"$MINIKUBE_IP"'/g' src/telegraf/telegraf.conf
 echo "All files set up correctly"
 
-#services="nginx mysql wordpress phpmyadmin ftps influxdb telegraf grafana"
-
 #docker build services
-docker build -t service_nginx ./nginx
-docker build -t service_ftps ./ftps
-docker build -t service_wordpress ./wordpress
-docker build -t service_telegraf ./telegraf
+docker build -t service_nginx src/nginx
+#docker build -t service_ftps src/ftps
+#docker build -t service_wordpress src/wordpress
+#docker build -t service_telegraf src/telegraf
+#docker build -t service_phpmyadmin src/phpmyadmin
+#docker build -t service_influxdb src/influxdb
+#docker build -t service_grafana src/grafana
+
+echo "Creating pods and services..."
+kubectl apply -f src/nginx.yaml
+#kubectl apply -f src/ingress.yaml
+
+echo "Opening the network in your browser"
+open http://$MINIKUBE_IP
